e3a161eb20efdf5936656cb10ee85db842c35c31
ManuelJansen <129184210+ManuelJansen@users.noreply.github.com>
2025-01-17 16:08:57 -0300

    17/01
    Recuperar Contraseña

--- src/app/recuperar-pass/recuperar-pass.page.ts
+++ src/app/recuperar-pass/recuperar-pass.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecuperarPassService } from '../Servicios/recuperar-pass.service';
 
 @Component({
   selector: 'app-recuperar-pass',
@@ -8,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPassPage implements OnInit {
 
-  constructor() { }
+  constructor(private recupService: RecuperarPassService) { }
+
+  numeroAleatorio: number|null = null;
+
+  correoEnviado = false;
+
+  generarNumero(): number|null{
+    return Math.floor(100000 + Math.random() * 900000);
+  };
 
   ngOnInit() {
   }
+  user = {
+    usuario: "",
+    password: "",
+    correo: "",
+  }
 
+  recuperar(){
+    if (this.user.usuario.length>0 && this.user.correo.length>0){
+      this.sendEmail(this.user.usuario, this.numeroAleatorio, this.user.correo);
+    };
+  };
+
+  async sendEmail(nombre: string, codigo: number|null, mail:string){
+    const formData = {
+      user_email: mail,
+      to_name: nombre,
+      codigo: codigo,
+    };
+    try{
+      const response = await this.recupService.sendEmail(formData);
+      this.correoEnviado = true;
+      console.log(response)
+    }catch (error){
+      console.error('Error enviando correo', error);
+    }
+  }
 }
