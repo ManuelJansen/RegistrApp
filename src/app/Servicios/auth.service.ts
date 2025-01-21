bd257932fbc746eec40affebc0a90c7ce1157d93
ManuelJansen <129184210+ManuelJansen@users.noreply.github.com>
2025-01-21 16:23:38 -0300

    local storage

--- src/app/Servicios/auth.service.ts
+++ src/app/Servicios/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +8,23 @@ export class AuthService {
 
   private static isLogged: boolean = false;
 
+  private storage: LocalStorageService = new LocalStorageService();
+
   constructor() { }
+
+  loginStorage(user: string, pass: string, tipo: number|null): boolean{
+    const listaUsuarios = this.storage.getItem('users') || [];
+
+    const conectado = listaUsuarios.find((userFind: any) =>
+       userFind.username == user && userFind.pass == pass && userFind.tipo == tipo);
+
+    if(conectado){
+      this.storage.setItem('conectado', conectado);
+      return true;
+    }else{
+      return false;
+    };
+  };
 
   login(user:string, pass:string):boolean{
     if (user=="j.riquelme" && pass=="pass1234"){
